f238caa31b1fe8f31623c650fe8cb05d9bd2c660
Natalia Velez Orjuela <jg6885567@gmail.com>
2026-05-18 11:55:27 -0500

    feat(SCRUM-23): implementar y documentar modelo de usuario con estados

--- servicios/bh-core/src/app.module.ts
+++ servicios/bh-core/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BasedatosModule } from './basedatos/basedatos.module';
+import { AutenticacionModule } from './modulos/autenticacion/autenticacion.module';
+import { CorreosModule } from './modulos/correos/correos.module';
+import { RolesModule } from './modulos/roles/roles.module';
+import { UsuariosModule } from './modulos/usuarios/usuarios.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    BasedatosModule,
+    AutenticacionModule,
+    UsuariosModule,
+    RolesModule,
+    CorreosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
