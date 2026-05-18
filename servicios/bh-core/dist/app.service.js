fbbe142c4c2c30755c62272811913c187a91b2b8
Juanjo2310 <jg6885567@gmail.com>
2026-05-18 10:07:26 -0500

    chore(SCRUM-71): crear estructura base de bh-core

--- servicios/bh-core/dist/app.service.js
+++ servicios/bh-core/dist/app.service.js
@@ -10,7 +10,7 @@ exports.AppService = void 0;
 const common_1 = require("@nestjs/common");
 let AppService = class AppService {
     getHello() {
-        return 'Hello World!';
+        return 'bh-core funcionando correctamente';
     }
 };
 exports.AppService = AppService;
