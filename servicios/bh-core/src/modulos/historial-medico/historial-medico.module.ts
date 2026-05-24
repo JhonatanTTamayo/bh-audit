fee17266776ad6134d389894e547743099bfe4ac
GuppyProg1 <alejo.diaz.ruiz@gmail.com>
2026-05-23 20:32:41 -0500

    fix: agregar JwtModule en HistorialMedicoModule y VacunasModule

--- servicios/bh-core/src/modulos/historial-medico/historial-medico.module.ts
+++ servicios/bh-core/src/modulos/historial-medico/historial-medico.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { BasedatosModule } from '../../basedatos/basedatos.module';
 import { HistorialMedicoController } from './historial-medico.controller';
 import { HistorialMedicoService } from './historial-medico.service';
@@ -8,7 +9,7 @@ import { HistorialMedicoService } from './historial-medico.service';
  * Módulo encargado de gestionar el historial médico de las mascotas.
  */
 @Module({
-  imports: [BasedatosModule, ConfigModule],
+    imports: [BasedatosModule, ConfigModule, JwtModule],
   controllers: [HistorialMedicoController],
   providers: [HistorialMedicoService],
   exports: [HistorialMedicoService],
--- servicios/bh-core/src/modulos/vacunas/vacunas.module.ts
+++ servicios/bh-core/src/modulos/vacunas/vacunas.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-
+import { JwtModule } from '@nestjs/jwt';
 import { BasedatosModule } from '../../basedatos/basedatos.module';
 import { VacunasController } from './vacunas.controller';
 import { VacunasService } from './vacunas.service';
@@ -9,7 +9,7 @@ import { VacunasService } from './vacunas.service';
  * Módulo encargado de gestionar las vacunas de las mascotas.
  */
 @Module({
-  imports: [BasedatosModule, ConfigModule],
+  imports: [BasedatosModule, ConfigModule, JwtModule],
   controllers: [VacunasController],
   providers: [VacunasService],
 })
